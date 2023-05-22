804eebae18a2828bfea7e9867b3784c3fcb93363
Ajit1428 <ajitthapa248@gmail.com>
2023-05-22 16:36:46 +0545

    Backend

--- Sapp.js
+++ Sapp.js
@@ -46,7 +46,7 @@ app.use((error, req, res, next)=>{
     })
 })
 
-app.listen(process.env.PORT || 2814, "localhost", (err) => {
+app.listen(process.env.PORT || 2814, process.env.HTTPPORT, (err) => {
     if(!err)
     {
     console.log("Server is listening to the port 2814")
